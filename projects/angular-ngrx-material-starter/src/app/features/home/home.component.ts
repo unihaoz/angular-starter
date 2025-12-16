42c877ad41630b77cd0c6d948d68530ed2acc268
unihaoz <unihaoz@gmail.com>
2025-12-16 06:39:17 +0000

    feat: add home component

--- projects/angular-ngrx-material-starter/src/app/features/home/home.component.ts
+++ projects/angular-ngrx-material-starter/src/app/features/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ROUTE_ANIMATIONS_ELEMENTS } from '../../core/animations/route.animations';
 
 @Component({
   selector: 'uh-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
+}
